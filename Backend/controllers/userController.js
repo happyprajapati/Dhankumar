62454da92f2fadcb4beb12dd9901b8f92c7969bc
Happy <104614758+happyprajapati@users.noreply.github.com>
2024-04-09 18:42:38 +0530

    update place order api

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -145,9 +145,9 @@ const placeOrder = async (req, res) => {
 const paymentVarify = async (req,res) => {
   try {
     const { razorpay_order_id, razorpay_payment_id, razorpay_signature } = req.body;
-    const expectedsign = crypto.hmac_sha256(razorpay_order_id + "|" + razorpay_payment_id, "LMyTuzcrjxYGilP394lXZxcD");;
-    // const exprctedSign = crypto.createHmac('sha256', 'rzp_test_MytonBdlQQC79x').update(sign.toString()).digest('hex');
-    if (razorpay_signature === expectedsign) {  
+    const sign = razorpay_order_id + '|' + razorpay_payment_id;
+    const exprctedSign = crypto.createHmac('sha256', 'rzp_test_MytonBdlQQC79x').update(sign.toString()).digest('hex');
+    if (razorpay_signature === exprctedSign) {  
       const newOrder = new Order({
         // itemid: req.body.itemid,
         // userid: req.body.userid,
@@ -156,7 +156,7 @@ const paymentVarify = async (req,res) => {
       })
       newOrder.save();
       // return res.json({ code: 200, success: true, msg: "Order Added." });
-      return res.json({ code: 200, success: true, msg: "Payment Successfull" });  
+      return res.json({ code: 200, success: true, msg: "Payment Successfull" });
     }else{
       return res.json({ code: 500, success: false, msg: "Invalid signature sent!" });
     }
